fb7fb2ce673203cf8563242421bae7eebae9ad43
raultocantins <alexbraul.ar@gmail.com>
2021-05-03 15:09:44 -0300

    adding workers for handle pdf

--- backend/api/app.js
+++ backend/api/app.js
@@ -6,6 +6,7 @@ const app=express()
 app.use(cors())
 app.use(Routes)
 app.use(express.json())
+app.set('view engine','ejs')
 app.use(function (req,res,next){
 	res.status(404).send("<h1>This is not the exist!</h1>");
 });
--- backend/api/routes.js
+++ backend/api/routes.js
@@ -1,6 +1,16 @@
 const express = require("express");
+
 const router = express.Router();
 const auth=require('./middlewares/token')
+const multer =require('multer')
+const upload=multer({dest:"uploads/", fileFilter: function (req, file, cb) {
+  if (file.originalname !== '.pdf') {
+    return cb(new Error('Only pdfs are allowed'))
+  }
+
+  cb(null, true)
+}})
+
 router.all('/*',[auth],(req,res,next)=>{ 
   next()
 })
@@ -20,6 +30,14 @@ router.get("/genres/:type/:id", (req, res) => {
   const history = req.params.id;
   res.send(`Irá devolver a historia ${history} do gênero ${type} `);
 });
+
+router.get('/index',(req,res)=>{
+  res.render('index')
+})
+router.post('/upload',upload.single('file'),(req,res)=>{
+  res.send('Upload com sucesso!!!')
+})
+
 
 
 module.exports = router;
