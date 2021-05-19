3ce7cf2edc1323937e1e4153a62051609f493b59
raultocantins <alexbraul.ar@gmail.com>
2021-05-19 14:50:07 -0300

    adding mockData

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -5,28 +5,22 @@ const upload=require('./multer')
 router.all("/*", [auth], (req, res, next) => {
   next();
 });
-
 router.get("/genres", (req, res) => {
   res.send("Irá devolver lista de gêneros de historias");
 });
-
 router.get("/genres/:type", (req, res) => {
   const type = req.params.type;
   res.send("Irá devolver todas as historias do tipo " + type);
 });
-
 router.get("/genres/:type/:id", (req, res) => {
   const type = req.params.type;
   const history = req.params.id;
   res.send(`Irá devolver a historia ${history} do gênero ${type} `);
 });
-
 router.get("/index", (req, res) => {
   res.render("index");
 });
-
 router.post("/upload", upload.single("file"), (req, res) => {
   res.send("Upload com sucesso!!!");
 });
-
 module.exports = router;
---
+++ backend/mock/mockData.js
@@ -0,0 +1,52 @@
+var genres = [
+    { id: "0", genreName: "natureza", genreImg: "https://drive.google.com/file/d/1gBMPRWihqqoONIvqAR8SKwm3R4oXFgGh/view?usp=sharing" },
+    { id: "1", genreName: "food", genreImg: "https://drive.google.com/file/d/1gBMPRWihqqoONIvqAR8SKwm3R4oXFgGh/view?usp=sharing" },
+  ];
+  var historys = [
+    {
+      id: "0",
+      historyName: "Acampamento",
+      historyImg: "https://drive.google.com/file/d/1gBMPRWihqqoONIvqAR8SKwm3R4oXFgGh/view?usp=sharing",
+      genreId: "0",
+      time: "45",
+      data: {
+        title: "Meu primeiro acampamento",
+        escritor: "Alex raul",
+        content: [
+          ["Minha primeira vez indo acampanhar com meus amigos foi super legal."],
+          ["Aprendi muitas coisas"],
+        ],
+      },
+    },
+    {
+      id: "1",
+      historyName: "Meu Macaco",
+      historyImg: "https://drive.google.com/file/d/10key0oc6Q0tdpHXGjkVaUwn0xkyFKPKp/view?usp=sharing",
+      genreId: "0",
+      time: "60",
+      data: {
+        title: "Meu amigo macaco",
+        escritor: "Alex raul",
+        content: [
+          ["Minha primeira vez indo acampanhar com meus amigos foi super legal."],
+          ["Aprendi muitas coisas"],
+        ],
+      },
+    },
+    {
+      id: "2",
+      historyName: "Almoço em familia",
+      historyImg: "https://drive.google.com/file/d/1DiEhGfjVrsHPX9ikc000xkXxACiPMJGp/view?usp=sharing",
+      genreId: "1",
+      time: "30",
+      data: {
+        title: "Familia barulhenta",
+        escritor: "Alex raul",
+        content: [
+          ["Minha primeira vez indo acampanhar com meus amigos foi super legal."],
+          ["Aprendi muitas coisas"],
+        ],
+      },
+    },
+  ];
+  
